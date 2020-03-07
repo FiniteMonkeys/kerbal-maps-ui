878ab47b7d391ac138c90703f6cb0348da3f35e6
Craig S. Cottingham <craig@cottingham.net>
2020-03-06 18:27:54 -0600

    Update various index.* files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MainMap extends React.Component {
+  render() {
+    return (
+      <div id="main-map" className="leaflet-sidebar-map"></div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ========================================
+
+ReactDOM.render(
+  <MainMap />,
+  document.getElementById("root")
+)
